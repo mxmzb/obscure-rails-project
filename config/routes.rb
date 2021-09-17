9f0392f938e9efa1182c29e4295b961d3d1cae63
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-17 22:14:57 +0700

    routes for the reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :cities
   root 'reviews#index'
 
-  get 'reviews/index'
+  get 'reviews', to: 'reviews#index'
   get 'reviews/new'
+  post 'reviews', to: 'reviews#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
