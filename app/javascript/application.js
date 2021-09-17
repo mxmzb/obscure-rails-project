dfe118c573394f3d4bdc14a6d71d61e736409912
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-17 13:31:13 +0700

    wip non-generic modal js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,27 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+
+
+// keep scope clean
+(() => {
+  const addReviewButton = document.getElementById("add-review-button");
+  const modal = document.getElementById("modal");
+  const modalContent = document.getElementById("modal-content");
+  
+  addReviewButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    fetch("/reviews/new")
+      .then((data) => data.text())
+      .then((html) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
+  
+        const newReview = doc.getElementById("new-review");
+        modal.classList.remove("hidden");
+        modalContent.appendChild(newReview);
+      });
+  });  
+})();
+
