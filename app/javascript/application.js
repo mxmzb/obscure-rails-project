2bed05aaa7ffaacff446451a2892e0656b9a96b3
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-17 14:04:14 +0700

    make the modal handler more generic

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,23 +5,32 @@
 
 // keep scope clean
 (() => {
-  const addReviewButton = document.getElementById("add-review-button");
-  const modal = document.getElementById("modal");
-  const modalContent = document.getElementById("modal-content");
-  
-  addReviewButton.addEventListener("click", (e) => {
+  const links = document.getElementsByTagName("a");
+
+  document.querySelector("a").addEventListener("click", (e) => {
     e.preventDefault();
 
-    fetch("/reviews/new")
-      .then((data) => data.text())
-      .then((html) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, "text/html");
-  
-        const newReview = doc.getElementById("new-review");
-        modal.classList.remove("hidden");
-        modalContent.appendChild(newReview);
-      });
+    const dataModal = e.target.getAttribute("data-modal");
+    const dataPath = e.target.getAttribute("data-path");
+    const dataElement = e.target.getAttribute("data-element");
+    const dataTarget = e.target.getAttribute("data-target");
+
+    if(dataModal === "open") {
+      fetch(dataPath)
+        .then(data => data.text())
+        .then(html => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+
+          // don't confuse: document is the current page document, doc is the 
+          // parsed doc from dataPath
+          const target = document.querySelector(dataTarget);
+          const element = doc.querySelector(dataElement);
+
+          target.appendChild(element);
+          modal.classList.remove("hidden");
+        });
+    }
   });  
 })();
 
