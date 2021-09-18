a602f909057f043922503e1a0c14abf73b9cbe10
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-18 11:36:18 +0700

    change root url to products index (coz if no products -> no reviews)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     post 'reviews', to: 'reviews#create'
   end
 
-  root 'reviews#index'
+  root 'products#index'
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
