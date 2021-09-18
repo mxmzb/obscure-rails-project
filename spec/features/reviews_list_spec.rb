97d4d66e04c476a015aaa8f0f8a13519e70c3d82
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-18 21:26:43 +0700

    create actually useful tests

--- spec/features/reviews_list_spec.rb
+++ spec/features/reviews_list_spec.rb
@@ -1,9 +1,46 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Visitor visits reviews', type: :feature do
-  scenario 'for first time' do
+RSpec.describe "User story", type: :feature do
+  it "can create a new product" do
     visit root_path
+    expect(page).to have_content("New product")
 
-    expect(page).to have_content('book was full of fluff')
+    click_on "New product"
+    expect(current_path).to eq("/products/new")
+
+    fill_in "Name", with: "The bad entrepreneur"
+    click_on "Create Product"
+
+    expect(current_path).to match(/\/products\/\d*/)
+    expect(page).to have_content("Product was successfully created.")
+  end
+
+  it "can show product reviews when no reviews have been written" do
+    product = Product.create(name: "The bad entrepreneur")
+
+    visit products_path
+    expect(page).to have_content("The bad entrepreneur")
+
+    find("#product_#{product.id}").click_link("show reviews")
+    expect(current_path).to match(/\/products\/\d*\/reviews/)
+    expect(page).to have_content("No reviews yet")
+    expect(page).to have_content("Add review")
+  end
+
+  it "can create a product review" do
+    product = Product.create(name: "The bad entrepreneur")
+
+    visit product_reviews_path(product)
+    expect(page).to have_content("The bad entrepreneur")
+
+    click_on "add-review-button"
+    expect(page).to have_selector("#new-review")
+
+    find(".star[data-value='4']").click
+    fill_in "review_text", with: "just fluff"
+
+    click_on "Submit review"
+    expect(page).to have_content("just fluff")
   end
 end
+
