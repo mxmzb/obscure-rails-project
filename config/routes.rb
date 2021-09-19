da4c53cb7ebb3604486475751723087d1b45ae2b
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-19 20:35:15 +0700

    add average rating route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products do
     get 'reviews', to: 'reviews#index'
     get 'reviews/new'
+    get 'reviews/avg_rating', to: 'reviews#avg_rating'
     post 'reviews', to: 'reviews#create'
   end
 
