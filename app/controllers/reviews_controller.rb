9037eb97f3a03225ea958792e5b688acb2f1188b
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2021-09-19 20:34:30 +0700

    render avg rating as a singular action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,16 @@ class ReviewsController < ApplicationController
     end
   end
 
+  # this is the same as index for now
+  def avg_rating
+    @reviews = @product.reviews
+    @avg_rating = nil
+
+    if @reviews.size > 0
+      @avg_rating = (@reviews.inject(0.0) { |sum, el| sum + el.rating }.to_f / @reviews.size).round(1)
+    end
+  end
+
   def new
     @review = Review.new(product: @product)
   end
